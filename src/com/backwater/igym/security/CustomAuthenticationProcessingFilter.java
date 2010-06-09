f7abd78d9add9424941126fbc9d96de55e1f7966
kkolla <kkolla@localhost>
2010-06-09 15:13:45 -0700

    Initial Import

---
+++ src/com/backwater/igym/security/CustomAuthenticationProcessingFilter.java
@@ -0,0 +1,33 @@
+package com.backwater.igym.security;
+
+import java.io.IOException;
+import java.io.Writer;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+public class CustomAuthenticationProcessingFilter extends UsernamePasswordAuthenticationFilter {
+
+	@Override
+	protected void successfulAuthentication(final HttpServletRequest request, final HttpServletResponse response,
+			final Authentication authResult) throws IOException, ServletException {
+		super.successfulAuthentication(request, response, authResult);
+		final Writer out = response.getWriter();
+		out.write("{success:true, targetUrl : ''}");
+		out.flush();
+	}
+
+	@Override
+	protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response,
+			AuthenticationException failed) throws IOException, ServletException {
+		super.unsuccessfulAuthentication(request, response, failed);
+		final Writer out = response.getWriter();
+		out.write("{ success: false, errors: { reason: 'Login failed. Try again.' }}");
+		out.flush();
+	}
+}
